9585c854e7bdb4f4047c6cd009ed227a9a4976ce
yinhao <764209393@qq.com>
2019-04-08 17:21:27 +0800

    first commit

---
+++ app/src/main/java/com/jake/drawtext/CustomView.java
@@ -0,0 +1,86 @@
+package com.jake.drawtext;
+
+import android.content.Context;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Paint.FontMetrics;
+import android.graphics.Paint.Style;
+import android.graphics.Point;
+import android.util.AttributeSet;
+import android.view.View;
+
+/**
+ * @author yinhao
+ * @date 2019/4/8
+ */
+public class CustomView extends View {
+
+  private Paint mPaint;
+  private Point mPoint;
+  private FontMetrics mFontMetrics;
+
+  public CustomView(Context context) {
+    super(context);
+    init();
+  }
+
+  public CustomView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    init();
+  }
+
+  public CustomView(Context context, AttributeSet attrs,
+      int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    init();
+  }
+
+  private void init() {
+    mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+    mPaint.setTextSize(sp2px(15));
+    mFontMetrics = new FontMetrics();
+    mPoint = new Point();
+    int[] location = new int[2];
+    getLocationOnScreen(location);
+    mPoint.x = location[0];
+    mPoint.y = location[1];
+  }
+
+  @Override
+  protected void onDraw(Canvas canvas) {
+    mPaint.setColor(Color.BLUE);
+    mPaint.setStyle(Style.STROKE);
+    mPaint.setStrokeWidth(10);
+    int width = getWidth();
+    int height = getHeight();
+    int r = Math.min(width, height) / 2 - 10;
+    float centerX = mPoint.x + width / 2;
+    float centerY = mPoint.y + height / 2;
+    //
+    canvas.drawCircle(centerX, centerY, r, mPaint);
+    //
+    mPaint.setColor(Color.RED);
+    canvas.drawLine(mPoint.x + width / 2, mPoint.y + height / 2 - r, mPoint.x + width / 2,
+        mPoint.y + height / 2 + r, mPaint);
+    canvas.drawLine(mPoint.x + width / 2 - r, mPoint.y + height / 2, mPoint.x + width / 2 + r,
+        mPoint.y + height / 2, mPaint);
+    //
+    mPaint.setColor(Color.WHITE);
+    mPaint.setStyle(Style.FILL);
+    String txt = "测试文件";
+    //测量文本宽度
+    float x = centerX - mPaint.measureText(txt) / 2;
+    mPaint.getFontMetrics(mFontMetrics);
+    //计算文本基线偏移位置
+    float baseLineOffsetY = (mFontMetrics.descent - mFontMetrics.ascent) / 2 - mFontMetrics.descent;
+    float baseLineY = centerY + baseLineOffsetY;
+    canvas.drawText(txt, x, baseLineY, mPaint);
+  }
+
+  private float sp2px(int spValue) {
+    float scaledDensity = Resources.getSystem().getDisplayMetrics().scaledDensity;
+    return spValue * scaledDensity + 0.5f;
+  }
+}
